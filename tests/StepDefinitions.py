b606e22b3f0d90e1b1b2586db6913c86cfe87cd5
Alaa Azazi <alaa.azazi@appdirect.com>
2020-02-10 21:54:22 -0700

    chore: add there_is_a_call_in_store step

--- tests/StepDefinitions.py
+++ tests/StepDefinitions.py
@@ -11,3 +11,10 @@ class StepDefinitions(unittest.TestCase):
 
     def generate_random_int(self, low=2, high=5):
         return random.randint(low, high)
+
+    def there_is_a_call_in_store(self, name=None, time=None):
+        _name = name or self.generate_random_string()
+        _time = time or self.generate_random_int()
+
+        self.store.update_result(_name, _time)
+        return _name, _time
