20eebc8505ebf02b43b2994b4610ff2673fa0e86
Alaa Azazi <alaa.azazi@appdirect.com>
2020-02-10 21:57:44 -0700

    chore: test update result one call

--- tests/_store/test_CacheStore.py
+++ tests/_store/test_CacheStore.py
@@ -8,3 +8,10 @@ class TestCacheStore(StepDefinitions):
 
     def tearDown(self):
         self.store.clear_results()
+
+    def test_update_result_one_call(self):
+        name, time = self.there_is_a_call_in_store()
+
+        result = self.store.get_result(name)
+        self.assertTrue(result)
+        self.assertEqual(result['time'], time)
