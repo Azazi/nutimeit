c671dbc745c2ae7c327143d4e92b99cbb88ec41a
Alaa Azazi <alaa.azazi@appdirect.com>
2020-02-10 21:53:36 -0700

    chore: add generate_random_int

--- tests/StepDefinitions.py
+++ tests/StepDefinitions.py
@@ -8,3 +8,6 @@ class StepDefinitions(unittest.TestCase):
         return ''.join(
             [random.choice(string.ascii_lowercase) for x in range(length)]
         )
+
+    def generate_random_int(self, low=2, high=5):
+        return random.randint(low, high)
